56e3559509b11e21829ff94ec5da9c1c437219c2
sushil kumar <sushil88081@gmail.com>
2024-11-15 00:51:47 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreens from "./src/screens/homeScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world this is sushil kumkdnqdqkn</Text>
+      <HomeScreens />
     </View>
   );
 }
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "flex-end",
+    justifyContent: "center",
   },
 });
---
+++ src/screens/homeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeScreens = () => {
+  return (
+    <>
+      <View>
+        <Text>Home Screen 1</Text>
+      </View>
+    </>
+  );
+};
+
+export default HomeScreens;
